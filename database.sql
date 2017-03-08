6de982330310a502ea6520b34eca6e57af0a3fa5
RandomTony <anthony.lohou@laposte.net>
2017-03-08 10:43:20 +0100

    Database sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+/******************************************************
+
+                .::SLQ code::.
+
+            @author Romain RINGENBACH
+            @author Anthony LOHOU
+
+            @date 8/03/2017
+
+            This database represents a digital bullet.
+
+
+******************************************************/
