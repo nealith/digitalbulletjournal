5fc27a6e033a4dfed8a7bc401f4969ea026e8b64
nealith <nealith@neaworld.fr>
2017-04-14 14:40:03 +0200

    update

--- API/libdbj.js
+++ API/libdbj.js
@@ -1,34 +1,3 @@
-/*  INTERFACE DB
- *
- *  query(sql, [param, ...], [callback])
- *  begin_transaction()
- *  commit_transaction()
- *  rollback_transaction()
- *  queries(callback)
- */
-
-
-var sqliteDB = function(database){
-
-    this.sqlite3 = require('sqlite3').verbose();
-    this.db = new sqlite3.Database('database');
-
-    this.query = db.run;
-    this.begin_transaction = function(){
-        this.query('BEGIN TRANSACTION');
-    }
-    this.commit_transaction = function(){
-        this.query('COMMIT;');
-    }
-    this.rollback_transaction = function(){
-        this.query('ROLLBACK;');
-    }
-    this.queries = function(callback){
-        this.db.serialize(callback);
-    }
-
-}
-
 var dbjDB = function(db){
 
     this.db = db;
