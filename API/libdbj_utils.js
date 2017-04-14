1981f5852c5125ef38a34c933c05372f52ebf60e
nealith <nealith@neaworld.fr>
2017-04-14 10:58:58 +0200

    Put utils on a separated lib

---
+++ API/libdbj_utils.js
@@ -0,0 +1,55 @@
+//==============================================================================
+//==============================================================================
+//
+// UTILS.
+//
+//==============================================================================
+//==============================================================================
+
+var utils = new function(){
+
+    this.check = new Object();
+
+    this.check.text = function(obj){
+        return true;
+    }
+
+    this.check.date = function(obj){
+        return true;
+    }
+
+    this.check.boolean = function(obj){
+        return true;
+    }
+
+    this.check.number = function(obj){
+        return true;
+    }
+
+    this.check.link = function(obj){
+        return true;
+    }
+
+    this.check.obj = function(obj){
+        return true;
+    }
+
+    this.check.user = function(obj){
+        return true;
+    }
+
+    this.check.log = function(obj){
+        return true;
+    }
+
+    this.check.topic = function(obj){
+        return true;
+    }
+
+    this.check.id = function(obj){
+        return true;
+    }
+
+    this.shasum = require('shasum');
+
+}
