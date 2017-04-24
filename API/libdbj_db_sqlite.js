75178994aa49415fc5ee5cceae17d28be967d566
nealith <nealith@neaworld.fr>
2017-04-24 21:50:35 +0200

    update db_sqlite to match utils : add function create, update, delete, select, that use utils

--- API/libdbj_db_sqlite.js
+++ API/libdbj_db_sqlite.js
@@ -1,22 +1,34 @@
 var sqliteDB = function(database){
 
+    this.utils = require('./libdbj_db_utils.js');
     this.sqlite3 = require('sqlite3').verbose();
     this.db = new sqlite3.Database('database');
 
-    this.query = db.run;
+    this.create = function(args,callback){
+        var query = this.utils.create(args);
+        this.sqlite3.run(query.sql,query.params,callback);
+    }
+    this.update = function(args,callback){
+        var query = this.utils.update(args);
+        this.sqlite3.run(query.sql,query.params,callback);
+    }
+    this.delete = function(args,callback){
+        var query = this.utils.delete(args);
+        this.sqlite3.run(query.sql,query.params,callback);
+    }
+    this.select = function(args,callback){
+        var query = this.utils.delete(args);
+        this.sqlite3.all(query.sql,query.params,callback);
+    }
     this.begin_transaction = function(){
-        this.query('BEGIN TRANSACTION');
+        this.sqlite3.run('BEGIN TRANSACTION');
     }
     this.commit_transaction = function(){
-        this.query('COMMIT;');
+        this.sqlite3.run('COMMIT;');
     }
     this.rollback_transaction = function(){
-        this.query('ROLLBACK;');
+        this.sqlite3.run('ROLLBACK;');
     }
-    this.queries = function(callback){
-        this.db.serialize(callback);
-    }
-
 }
 
 module.exports = sqliteDB;
