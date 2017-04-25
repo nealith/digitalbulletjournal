79f8a5089bbf88632c5cd020033a4aa6fa50e579
nealith <nealith@neaworld.fr>
2017-04-25 14:29:55 +0200

    Add statement object that allow transactions

--- API/libdbj_db_sqlite.js
+++ API/libdbj_db_sqlite.js
@@ -4,31 +4,95 @@ var sqliteDB = function(database){
     this.sqlite3 = require('sqlite3').verbose();
     this.db = new sqlite3.Database('database');
 
-    this.insert = function(args,callback){
-        var query = this.utils.create(args);
-        this.sqlite3.run(query.sql,query.params,callback);
+    var DB_TRANSACTION_STATEMENT = function(db,utils,transaction){
+
+        this.db = db;
+        this.utils = utils;
+        this.sql = '';
+        this.callback = null;
+        this.lock = false;
+        if (transaction) {
+            sql+='BEGIN TRANSACTION;'
+        }
+
     }
-    this.update = function(args,callback){
-        var query = this.utils.update(args);
-        this.sqlite3.run(query.sql,query.params,callback);
+
+    DB_TRANSACTION_STATEMENT.prototype.insert = function (args) {
+        if (!lock) {
+            var query = this.utils.insert(args);
+            var sql = this.utils.to_sql(query);
+            this.sql += sql;
+        }
     }
-    this.delete = function(args,callback){
-        var query = this.utils.delete(args);
-        this.sqlite3.run(query.sql,query.params,callback);
+
+    DB_TRANSACTION_STATEMENT.prototype.update = function (args) {
+        if (!lock) {
+            var query = this.utils.update(args);
+            var sql = this.utils.to_sql(query);
+            this.sql += sql;
+        }
     }
-    this.select = function(args,callback){
-        var query = this.utils.delete(args);
-        this.sqlite3.all(query.sql,query.params,callback);
+
+    DB_TRANSACTION_STATEMENT.prototype.delete = function (args) {
+        if (!lock) {
+            var query = this.utils.delete(args);
+            var sql = this.utils.to_sql(query);
+            this.sql += sql;
+        }
     }
-    this.begin_transaction = function(){
-        this.sqlite3.run('BEGIN TRANSACTION');
+
+    DB_TRANSACTION_STATEMENT.prototype.select = function (args) {
+        if (!lock) {
+            var query = this.utils.select(args);
+            var sql = this.utils.to_sql(query);
+            this.sql += sql;
+        }
     }
-    this.commit_transaction = function(){
-        this.sqlite3.run('COMMIT;');
+
+    DB_TRANSACTION_STATEMENT.prototype.exec = function (callback) {
+        if (!lock) {
+            lock = true;
+            this.callback = callback;
+            var callback = this.callback;
+            if (transaction) {
+                callback = this.finalize;
+            }
+            this.db.exec(this.sql,callback);
+        }
     }
-    this.rollback_transaction = function(){
-        this.sqlite3.run('ROLLBACK;');
+    DB_TRANSACTION_STATEMENT.prototype.exec = function (err,args) {
+        if (lock) {
+            if (!err) {
+                this.db.run('COMMIT;');
+            } else {
+                this.db.run('ROLLBACK;');
+            }
+            this.callback(err,null);
+        }
     }
+
+
+}
+
+this.prototype.insert = function(args,callback){
+    var query = this.utils.create(args);
+    this.sqlite3.run(query.sql,query.params,callback);
+}
+this.prototype.update = function(args,callback){
+    var query = this.utils.update(args);
+    this.sqlite3.run(query.sql,query.params,callback);
+}
+this.prototype.delete = function(args,callback){
+    var query = this.utils.delete(args);
+    this.sqlite3.run(query.sql,query.params,callback);
+}
+this.prototype.select = function(args,callback){
+    var query = this.utils.delete(args);
+    this.sqlite3.all(query.sql,query.params,callback);
+}
+
+this.prototype.stmt = function(){
+    return new DB_TRANSACTION_STATEMENT(this.db,this.utils);
 }
 
 module.exports = sqliteDB;
