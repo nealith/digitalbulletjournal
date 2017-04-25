6dfd07ad5a4958fffa4bdd5a4e5f0cb6527d1a46
RandomTony <anthony.lohou@laposte.net>
2017-04-25 21:33:12 +0200

    Create logs

--- API/dao_log.js
+++ API/dao_log.js
@@ -8,8 +8,39 @@ var DAO_Logs = function(db,id){
 }
 
 
-DAO_Logs.prototype.create = function(args,stmt){
+DAO_Logs.prototype.erase = function (err,dao) {
+    if (!err) {
+        this = dao;
+    }
+}
 
+DAO_Logs.prototype.create = function(owner,privacy,title,callback){
+
+    dao = new DAO_Logs(this.db);
+    dao.callback = callback;
+    dao.creation_date = Date.now();
+    dao.owner = owner;
+    dao.title = title;
+    dao.privacy = privacy;
+    var stmt = this.db.stmt(true);
+    stmt.insert({
+        table:'Logs',
+        keys:null,
+        values:{
+            creation_date:dao.creation_date,
+            owner:dao.owner,
+            title:dao.title,
+            privacy:dao.privacy
+        }
+    });
+    stmt.exec();
+}
+
+DAO_Logs.prototype.create_callback = function (err,args) {
+
+    if (!err) {
+        this.callback(err,this);
+    }
 }
 
 DAO_Logs.prototype.update = function(stmt,callback){
