f720313c4f8db41686748eab250ea17b238394a3
nealith <nealith@neaworld.fr>
2017-04-28 23:04:09 +0200

    remove old files

--- API/libdbj.js
+++
@@ -1,9 +0,0 @@
-var dbjDB = function(db){
-
-    this.db = db;
-    //this.all_queries = JSON.parse(fs.readFileSync('queries.json', 'utf8'));
-    this.data = require('./libdbj_data.js');
-    this.ult = require('./libdbj_ult.js');
-    this.utils = require('./libdbj_utils.js');
-
-}
