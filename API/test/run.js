c5e5fe8c9a9f1d2de2c3f7e8fccbabc14538a5b1
nealith <nealith@neaworld.fr>
2017-05-08 00:09:39 +0200

    add function to fill a database

--- API/test/run.js
+++ API/test/run.js
@@ -1,3 +1,5 @@
+var DT_API = require('../api.js');
+
 var run_count = 0;
 var fail_count = 0;
 
@@ -8,13 +10,229 @@ var dao_log_user;
 var dao_data;
 
 // FULL DATA BASE
+
+function full_db(callback){
     // Create users
+
+    var users = new Array();
+
+    function create_users(callback){
+        var m = 0;
+        var n = 5;
+        function create_user(err,args){
+            if (!err) {
+                users.push(args);
+                m++;
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    dao_user.create('user'+m,'user'+m,'user'+m,'user'+m,'user'+m+'@users.com',create_user);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        dao_user.create('user'+m,'user'+m,'user'+m,'user'+m,'user'+m+'@users.com',create_user);
+    }
     // Create logs
+
+    var logs = new Array();
+
+    function create_logs(callback){
+        var m = 0;
+        var n = 5;
+        function create_log(err,args){
+            if (!err) {
+                logs.push(args);
+                m++;
+
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    dao_log.create(users[m].id,true,'title'+m,create_log);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        dao_log.create(users[m].id,true,'title'+m,create_log);
+    }
+
     // Share logs
+
+    function share_logs(callback){
+        callback(null,null);
+    }
+
     // Create topics
+
+    var topics = new Array();
+
+    function create_topics(callback){
+        var m = 0;
+        var n = 5;
+        function create_topic(err,args){
+            if (!err) {
+                topics.push(args);
+                m++;
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    dao_topic.create(logs[m].id,'title'+m,create_topic);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        dao_topic.create(logs[m].id,'title'+m,create_topic);
+    }
+
     // Create simples data
+
+    function create_simples_data(callback){
+        var m = 0;
+        var n = 5;
+        function create_data(err,args){
+            if (!err) {
+                m++;
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    dao_data.create(topics[m].id,users[m].id,'Text','lol'+m,create_data);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        dao_data.create(topics[m].id,users[m].id,'Text','test'+m,create_data);
+    }
+
     // Create compound data
+
+    function create_compounds_data(callback){
+        var m = 0;
+        var n = 5;
+
+        function new_composite(no){
+            var value = new Object();
+
+            for (var i = 0; i < 5; i++) {
+                value['t'+i] = new DT_API.DAO_DATA(dao_data.db,null,null,topics[m],users[m],'Text','test_c'+i,null);
+            }
+
+            if (no < m) {
+                value['t5'] = new_composite(5);
+            }
+
+            var dao = new DT_API.DAO_DATA(dao_data.db,null,null,topics[m],users[m],'Compound',value,null);
+
+
+
+            return dao;
+        }
+
+        function create_data(err,args){
+            if (!err) {
+                m++;
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    new_composite(m).update(create_data);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        new_composite(m).update(create_data);
+    }
+
     // Create models data
+
+    function create_models_data(callback){
+        var m = 0;
+        var n = 5;
+
+        function new_model(no){
+            var value = new Object();
+
+            for (var j = 0; j < 5; j++) {
+                value['s'+j] = new Array();
+                for (var i = 0; i < 5; i++) {
+                    value['s'+j]['t'+i] = new DT_API.DAO_DATA(dao_data.db,null,null,topics[m],users[m],'Text','test_c'+i,null);
+                }
+                if (no < m) {
+                    value['s'+j]['t5'] = new_composite(5);
+                }
+
+            }
+            var dao = new DT_API.DAO_DATA(dao_data.db,null,null,topics[m],users[m],'Model',value,null);
+
+            return dao;
+        }
+
+        function create_data(err,args){
+            if (!err) {
+                m++;
+                if (m == n) {
+                    callback(err,args);
+                } else {
+                    new_model(m).update(create_data);
+                }
+
+            } else {
+                callback(err,args);
+            }
+        }
+        new_model(m).update(create_data);
+    }
+
+    create_users(function(err,args){
+        if (!err) {
+            create_logs(function(err,args){
+                if (!err) {
+                    create_topics(function(err,args){
+                        if (!err) {
+                            share_logs(function(err,args){
+                                if (!err) {
+                                    create_simples_data(function(err,args){
+                                        if (!err) {
+                                            create_compounds_data(function(err,args){
+                                                if (!err) {
+                                                    create_modeles_data(function(err,args){
+                                                        callback(err,args);
+                                                    });
+                                                } else {
+                                                    callback(err,args)
+                                                }
+                                            });
+                                        } else {
+                                            callback(err,args)
+                                        }
+                                    });
+                                } else {
+                                    callback(err,args)
+                                }
+                            });
+                        } else {
+                            callback(err,args)
+                        }
+                    });
+                } else {
+                    callback(err,args)
+                }
+            });
+        } else {
+            callback(err,args)
+        }
+    });
+}
+
+
 
 // USER TESTS
     // Create a user with create()
@@ -90,24 +308,27 @@ var dao_data;
 
 
 
-var DT_API = require('../api.js');
 
-dao_user = new DT_API.DAO_USER(db);
-dao_log = new DT_API.DAO_LOG(db);
-dao_topic = new DT_API.DAO_TOPIC(db);
-dao_log_user = new DT_API.DAO_LOG_USER(db);
-dao_data = new DT_API.DAO_DATA(db);
+
+
 
 var SQLITE_DB = require('../libdbj_db_sqlite.js');
 
 var fs = require('fs');
 
-var script = fs.readFileSync('../database_v2.sql', 'utf8');
+var script = fs.readFileSync('./database_v2.sql', 'utf8');
 
 var sqlite_db = new SQLITE_DB(':memory:');
+dao_user = new DT_API.DAO_USER(sqlite_db);
+dao_log = new DT_API.DAO_LOG(sqlite_db);
+dao_topic = new DT_API.DAO_TOPIC(sqlite_db);
+dao_log_user = new DT_API.DAO_LOG_USER(sqlite_db);
+dao_data = new DT_API.DAO_DATA(sqlite_db);
 sqlite_db.db.exec(script,function(err,args){
     if (!err) {
-        run_test_on(sqlite_db);
+        full_db(function(err,args){
+            console.log(err,args);
+        });
     } else {
         console.log(err);
     }
